b213957d04a22645c58707ab03442ad29c56897e
aakilahamed2005 <aakilahamed2005@gmail.com>
2025-06-06 22:42:33 +0530

    Update file

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const tasksContainer = document.querySelector('.tasks');
     const inputField = document.querySelector('.input-field');
     const windowWidthFixed = window.innerWidth;
+
+    if(windowWidthFixed < 400){
+        document.querySelector('.card').style.margin = "0px";
+    }
     
 
     submitBtn.style.display = 'none';
@@ -23,38 +27,40 @@ document.addEventListener('DOMContentLoaded', ()=>{
     
 
     form.addEventListener("submit", (event)=>{
+        //Preventing from submitting the form 
         event.preventDefault();
+        //Getting the text that was in input field
         const newTask = document.querySelector('.input-field').value;
-        console.log(newTask);
         
         //clear the input field soon as the form is submitted
         document.querySelector('.input-field').value = '';
 
+        //Create the elements
         const taskBox = document.createElement('div');
         const task = document.createElement('div');
         const closeBtn = document.createElement('button');
 
+        //Adding class names for the above created elements
         taskBox.className = 'task-box';
         task.className = 'task';
         closeBtn.className = 'close-btn';
 
+        //assigning the values to the html code
         task.innerHTML = newTask;
         closeBtn.innerHTML = 'X';
 
+        //remove the above all created elements if the close button is clicked
         closeBtn.addEventListener('click',()=>{
             taskBox.remove();
             task.remove();
             closeBtn.remove();
         });
         
+        //appending the element to parent element
         taskBox.append(task, closeBtn);
         tasksContainer.append(taskBox);
-
-    
-
-        return false;
     })
 
-    
+
 
 });
