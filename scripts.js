7972f83d1d999e6751865ea64fe4850e6be4162a
aakilahamed2005 <aakilahamed2005@gmail.com>
2025-06-06 20:00:42 +0530

    items adding to list

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     document.querySelector('.input-field').onkeyup = ()=>{
         if(document.querySelector('.input-field').value.length >0){
-            submit_btn.style.transition = '1s';
             submit_btn.style.display = 'block';
         }else{
             submit_btn.style.display = 'none';
@@ -18,9 +17,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
     
 
     form.onsubmit = ()=>{
-        const task = document.querySelector('.input-field').value;
+        const newtask = document.querySelector('.input-field').value;
         //clear the input field soon as the form is submitted
         document.querySelector('.input-field').value = '';
+
+        const task_box = document.createElement('div');
+        const task = document.createElement('div');
+        const close_btn = document.createElement('button');
+
+        task_box.className = 'task-box';
+        task.className = 'task';
+        close_btn.className = 'close-btn';
+
+        document.querySelector('.tasks').append(task_box);
+        task_box.append(task);
+        task_box.append(close_btn);
+
+        task.innerHTML = newtask;
+        close_btn.innerHTML = 'X';
+
+        close_btn.onclick = ()=>{
+            close_btn.remove();
+            task.remove();
+            task_box.remove();
+        };
+
+        document.querySelector('tasks')
         return false;
     };
 
